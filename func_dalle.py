161127ea7eba611530342010a4d239159e0dcaf5
jrr997 <zackchangjx@foxmail.com>
2023-10-27 17:05:07 +0800

    fix: openAI多账号管理

--- func_dalle.py
+++ func_dalle.py
@@ -3,17 +3,21 @@ import openai
 class DALLE:
   # def __init__(self, key: str, api: str, proxy: str) -> None:
   def __init__(self, api, key, version, type) -> None:
-        openai.api_type = type
-        openai.api_base = api
-        openai.api_version = version
-        openai.api_key = key
+        self.api_type = type
+        self.api_base = api
+        self.api_version = version
+        self.api_key = key
 
   def generate_images(self, prompt):
     try:
       response = openai.Image.create(
       prompt=prompt,
       size='1024x1024',
-      n=1
+      n=1,
+      api_type = self.api_type,
+      api_base = self.api_base,
+      api_version = self.api_version,
+      api_key = self.api_key,
       )
       image_url = response["data"][0]["url"]
       return image_url
